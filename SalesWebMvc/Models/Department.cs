4c98f3b1588ca845fdc88d01b2514780d6ef43f2
Gabriel <gabrieljr2307@gmail.com>
2020-04-12 11:19:15 -0300

    First commit

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public void RemoveSeller(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+
+        public Double TotalSales(DateTime initial, DateTime finali)
+        {
+            return Sellers.Sum(x => x.TotalSales(initial, finali));
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        // Add sales one in ICollerctio SalesRecord List
+        public void AddSales(SalesRecord sales)
+        {
+            Sales.Add(sales);
+        }
+        // Remove sales one in ICollerctio SalesRecord List
+        public void RemoveSales(SalesRecord sales)
+        {
+            Sales.Remove(sales);
+        }
+        
+        // Total Sales
+        public Double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(x => x.Date >= initial && x.Date <= final)
+                        .Sum(x => x.Amount);
+        }
+
+
+    }
+}
