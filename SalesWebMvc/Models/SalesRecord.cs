05db4faf21c21415ef7e890003def9ee393d7184
Gabriel <gabrieljr2307@gmail.com>
2020-04-13 20:42:28 -0300

    Finally

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWebMvc.Models.Enuns;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
@@ -17,7 +18,9 @@ namespace SalesWebMvc.Models
         }
 
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public Double Amount { get; set; }
         public SaleStatus SaleStatus { get; set; }
         public Seller Seller { get; set; }
